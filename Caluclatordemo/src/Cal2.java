d78cf7af3ee8f6a9aa920b75ab8c7a508bd16eb7
Mahesh <Mahesh@LAPTOP-B2TKSRKV.frontierlocal.net>
2018-01-17 16:16:25 -0600

    New Caluclator code

---
+++ Caluclatordemo/src/Cal2.java
@@ -0,0 +1,11 @@
+
+public class Cal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Caluclator1 c=new Caluclator1();
+ System.out.println(c.add(5, 4));
+ 
+	}
+
+}
---
+++ Caluclatordemo/src/Caluclator1.java
@@ -0,0 +1,14 @@
+
+public class Caluclator1 {
+
+	public int add(int Num1, int Numb2) {
+		int r=Num1+Numb2;
+		return r;
+	}
+
+	public int sub(int Num1, int Numb2) {
+		int r1=Num1-Numb2;
+		return r1;
+		
+	}
+}
